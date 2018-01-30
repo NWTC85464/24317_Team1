98ff80e2967586b180a55451be26c6d32fc04233
AntonTurba <anton.turba@mymail.nwtc.edu>
2018-01-29 19:25:12 -0600

    Added message box

--- Test2/Test2/Form1.cs
+++ Test2/Test2/Form1.cs
@@ -16,5 +16,12 @@ namespace Test2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("It works");
+            MessageBox.Show("adsfsa");
+            MessageBox.Show("test");
+        }
     }
 }
