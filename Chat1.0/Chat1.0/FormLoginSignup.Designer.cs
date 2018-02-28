e41371e8a56ae397618965136b28b170c730e677
Anton <antondavidturba@gmail.com>
2018-02-28 11:30:00 -0600

    Added LoginSignup form

---
+++ Chat1.0/Chat1.0/FormLoginSignup.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Chat1._0
+{
+    partial class FormLoginSignup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblDisplayUsernameError = new System.Windows.Forms.Label();
+            this.lblDisplayPasswordError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(111, 111);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(96, 23);
+            this.btnSignUp.TabIndex = 0;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(213, 111);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(96, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(9, 111);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(96, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(74, 36);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(74, 74);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 4;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(10, 39);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(58, 13);
+            this.lblUserName.TabIndex = 5;
+            this.lblUserName.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(13, 77);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblDisplayUsernameError
+            // 
+            this.lblDisplayUsernameError.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplayUsernameError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayUsernameError.Location = new System.Drawing.Point(191, 36);
+            this.lblDisplayUsernameError.Name = "lblDisplayUsernameError";
+            this.lblDisplayUsernameError.Size = new System.Drawing.Size(118, 20);
+            this.lblDisplayUsernameError.TabIndex = 7;
+            // 
+            // lblDisplayPasswordError
+            // 
+            this.lblDisplayPasswordError.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplayPasswordError.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayPasswordError.Location = new System.Drawing.Point(191, 74);
+            this.lblDisplayPasswordError.Name = "lblDisplayPasswordError";
+            this.lblDisplayPasswordError.Size = new System.Drawing.Size(118, 20);
+            this.lblDisplayPasswordError.TabIndex = 8;
+            // 
+            // FormLoginSignup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 141);
+            this.Controls.Add(this.lblDisplayPasswordError);
+            this.Controls.Add(this.lblDisplayUsernameError);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnSignUp);
+            this.Name = "FormLoginSignup";
+            this.Text = "FormLoginSignup";
+            this.Load += new System.EventHandler(this.FormLoginSignup_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblDisplayUsernameError;
+        private System.Windows.Forms.Label lblDisplayPasswordError;
+    }
+}
---
+++ Chat1.0/Chat1.0/FormLoginSignup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat1._0
+{
+    public partial class FormLoginSignup : Form
+    {
+        public FormLoginSignup()
+        {
+            InitializeComponent();
+        }
+        //TODO Add verify password field and associated code for sign up attempts
+
+        private void FormLoginSignup_Load(object sender, EventArgs e)
+        {
+            // I have these hidden on form load instead of on design view
+            // so the labels can be seen and modified without going under all controls
+            lblDisplayUsernameError.Visible = false;
+            lblDisplayPasswordError.Visible = false;
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            // Resize the form to allow for addition of a verify password field
+            // when the user selects sign up 
+            this.Height += 200;
+        }
+    }
+}
