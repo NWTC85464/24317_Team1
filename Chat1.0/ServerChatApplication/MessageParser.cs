2de73d31ff2bd9b3ac59f9449cc9539737afa381
Anton <antondavidturba@gmail.com>
2018-04-12 16:54:57 -0500

    Added a class for parsing incoming messages

---
+++ Chat1.0/ServerChatApplication/MessageParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerChatApplication
+{
+    public static class MessageParser
+    {
+        // Holds the original message that's sent to the server before any tokenizing occurs
+        private static string completeMessage;
+
+        // Holds the tokenized message
+        private static string[] tokenizedMessage;
+
+        // Get and set for the completeMessage field. Once a new value is set, the tokenize method is called
+        public static string CompleteMessage
+        {
+            get { return completeMessage; }
+            set
+            {
+                completeMessage = value;
+                TokenizeMessage();
+            }
+        }
+
+        // Tokenize the original message and redirect the program flow
+        private static void TokenizeMessage()
+        {
+            tokenizedMessage = completeMessage.Split('|');
+
+            switch (tokenizedMessage[0])
+            {
+                case "<Message>":
+                    // TODO setup appropriate methods
+                    break;
+                case "<Login>":
+                    // TODO setup appropriate methods
+                    break;
+                case "<Signup>":
+                    // TODO setup appropriate methods
+                    break;
+            }
+        }
+    }
+}
