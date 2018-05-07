2dad24df8b932e293d0e3e61c11193e33eb75de8
Mohammed Hossain <mohammed.hossain@mymail.nwtc.edu>
2018-05-07 23:14:09 +0500

    updated formloginsignup.cs & socketControler.cs

--- Chat1.0/Chat1.0/FormLoginSignup.cs
+++ Chat1.0/Chat1.0/FormLoginSignup.cs
@@ -16,6 +16,9 @@ namespace Chat1._0
         TextBox txtPasswordConfirm;
         Label lblPasswordConfirm;
 
+        //boolean for signup label
+        bool signup = false;
+
         public FormLoginSignup(SocketController sctctrl)
         {
             InitializeComponent();
@@ -32,12 +35,15 @@ namespace Chat1._0
         {
             // I have these hidden on form load instead of on design view
             // so the labels can be seen and modified without going under all controls
-            lblDisplayUsernameError.Visible = false;
-            lblDisplayPasswordError.Visible = false;
+            //lblDisplayUsernameError.Visible = false;
+            //lblDisplayPasswordError.Visible = false;
         }
 
         private void btnSignUp_Click(object sender, EventArgs e)
         {
+            //set sign up to true
+            signup = true;
+
             // Resize the form to allow for addition of a verify password field
             // when the user selects sign up 
             this.Height += 31;
@@ -54,7 +60,7 @@ namespace Chat1._0
             this.lblPasswordConfirm = new Label();
             this.lblPasswordConfirm.Width = 89;
             this.lblPasswordConfirm.Height = 45;
-            p = new Point(12,112);
+            p = new Point(12, 112);
             this.lblPasswordConfirm.Location = p;
             this.lblPasswordConfirm.Text = "Confirm Password:";
             this.lblPasswordConfirm.Font = new Font("Arial Black", 10, FontStyle.Bold);
@@ -64,7 +70,7 @@ namespace Chat1._0
             this.txtPasswordConfirm = new TextBox();
             this.txtPasswordConfirm.Width = 100;
             this.txtPasswordConfirm.Height = 20;
-            p = new Point(103, 124);
+            p = new Point(103, 128);
             this.txtPasswordConfirm.Location = p;
             this.txtPasswordConfirm.PasswordChar = '*';
             this.Controls.Add(txtPasswordConfirm);
@@ -79,27 +85,49 @@ namespace Chat1._0
         // New signup method button click
         private void btnSignUp_SecondClick(object sender, EventArgs e)
         {
-            if(txtPassword.Text == txtPasswordConfirm.Text)
+            //Empty error message texts
+            lblDisplayPasswordError.Text = String.Empty;
+            lblDisplayUsernameError.Text = String.Empty;
+
+            //Username and password validation
+            if (!string.IsNullOrWhiteSpace(txtUsername.Text))
             {
-                if (sctctrl.SendUserSignUpRequest(this.txtUsername.Text, this.txtPassword.Text))
+                if (!string.IsNullOrWhiteSpace(txtPassword.Text))
                 {
-                    this.FormClosing -= this.loginForm_FormClosing;
-                    this.Close();
+                    if (txtPassword.Text == txtPasswordConfirm.Text)
+                    {
+                        if (sctctrl.SendUserSignUpRequest(this.txtUsername.Text, this.txtPassword.Text))
+                        {
+                            this.FormClosing -= this.loginForm_FormClosing;
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Sign up Unsuccessful.");
+                        }
+                    }
+                    else
+                    {
+                        lblDisplayPasswordError.Text = "Passwords Must Match";
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Sign up Unsuccessful.");
+                    lblDisplayPasswordError.Text = "Enter Password";
                 }
             }
             else
             {
-                MessageBox.Show("Passwords don't match.");
+                lblDisplayUsernameError.Text = "Enter Username";
             }
         }
 
         // New Login button click to reset the form;
         private void btnLogin_SecondClick(object sender, EventArgs e)
         {
+            //set signup to false
+            signup = false;
+
             this.Height -= 31;
             Point p = new Point(9, 131);
             this.btnExit.Location = p;
@@ -129,26 +157,49 @@ namespace Chat1._0
         // Login Button Logs user into server after checking username and password
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            //if (sctctrl.SendUserLoginRequest(txtUsername.Text, txtPassword.Text))
-            //{
-            //    //Login successful-Open Chat Manager
-            //    this.FormClosing -= this.loginForm_FormClosing;
-            //    this.Close();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Login Unsuccessful.");
+            //Empty error message texts
+            lblDisplayPasswordError.Text = String.Empty;
+            lblDisplayUsernameError.Text = String.Empty;
+            // Username and password validation
+            if (!string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                if (!string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
 
-            //    //For now have login go to chat manager for testing purposes
-            //    //TODO remove before usage or when testing signup/login
-            //    this.FormClosing -= this.loginForm_FormClosing;
-            //    this.Close();
-            //}
+                    if (sctctrl.SendUserLoginRequest(txtUsername.Text, txtPassword.Text))
+                    {
+                        //Login successful-Open Chat Manager
+                        this.FormClosing -= this.loginForm_FormClosing;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login Unsuccessful.");
 
-            //for testing-delete when need to test login
-            // This removes the application.exit event handler from the formclosing event;
-            this.FormClosing -= this.loginForm_FormClosing;
-            this.Close();
+                        //For now have login go to chat manager for testing purposes
+                        //TODO remove before usage or when testing signup/login
+                        this.FormClosing -= this.loginForm_FormClosing;
+                        this.Close();
+                    }
+                    //for testing-delete when need to test login
+                    // This removes the application.exit event handler from the formclosing event;
+                    this.FormClosing -= this.loginForm_FormClosing;
+                    this.Close();
+                }
+                else
+                {
+                    lblDisplayPasswordError.Text = "Enter Password";
+                }
+
+            }
+            else
+            {
+                lblDisplayUsernameError.Text = "Enter Username";
+            }
+
+
+
+
         }
 
 
@@ -156,10 +207,61 @@ namespace Chat1._0
         private void pwd_show_chk_CheckedChanged(object sender, EventArgs e)
         {
             txtPassword.PasswordChar = (pwd_show_chk.Checked ? '\0' : '*');
+            txtPasswordConfirm.PasswordChar = (pwd_show_chk.Checked ? '\0' : '*');
         }
         private void loginForm_FormClosing(Object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (signup == true)
+            {
+                //empty label when password starts typing
+                lblDisplayPasswordError.Text = String.Empty;
+
+                //show password strengh depending on length and digits
+                if (!string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
+                    if (txtPassword.Text.Length > 8 && txtPassword.Text.Any(char.IsDigit))
+                    {
+                        lblDisplayPasswordError.BackColor = Color.Green;
+                        lblDisplayPasswordError.Text = "STRONG";
+                        lblDisplayPasswordError.ForeColor = Color.White;
+                        lblDisplayPasswordError.Font = new Font("Arial Black", 10, FontStyle.Bold);
+                    }
+                    else if (txtPassword.Text.Length > 4)
+                    {
+                        lblDisplayPasswordError.BackColor = Color.Gold;
+                        lblDisplayPasswordError.Text = "OKAY";
+                        lblDisplayPasswordError.ForeColor = Color.White;
+                        lblDisplayPasswordError.Font = new Font("Arial Black", 10, FontStyle.Bold);
+                    }
+                    else
+                    {
+                        lblDisplayPasswordError.BackColor = Color.DarkRed;
+                        lblDisplayPasswordError.Text = "WEAK";
+                        lblDisplayPasswordError.ForeColor = Color.White;
+                        lblDisplayPasswordError.Font = new Font("Arial Black", 10, FontStyle.Bold);
+                    }
+                }
+                else
+                {
+                    lblDisplayPasswordError.BackColor = SystemColors.GradientActiveCaption;
+                    lblDisplayPasswordError.Text = String.Empty;
+                    lblDisplayPasswordError.ForeColor = Color.DarkRed;
+                    lblDisplayPasswordError.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Regular);
+
+                }
+            }
+            else
+            {
+                lblDisplayPasswordError.BackColor = SystemColors.GradientActiveCaption;
+                lblDisplayPasswordError.Text = String.Empty;
+                lblDisplayPasswordError.ForeColor = Color.DarkRed;
+                lblDisplayPasswordError.Font = new Font("Microsoft Sans Serif", 8, FontStyle.Regular);
+            }
         }
     }
 }
