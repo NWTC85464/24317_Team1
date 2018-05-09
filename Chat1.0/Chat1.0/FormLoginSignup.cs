89b007715e4671035d4e71da96f7a15f10827636
Aaron <kahanam24@gmail.com>
2018-05-09 18:27:07 -0500

    Fixed FormLoginSignup Validation

--- Chat1.0/Chat1.0/FormLoginSignup.cs
+++ Chat1.0/Chat1.0/FormLoginSignup.cs
@@ -105,9 +105,25 @@ namespace Chat1._0
             {
                 lblDisplayUsernameError.Text = "Enter Username";
             }
+            else if(txtUsername.Text.Length <= 5)
+            {
+                lblDisplayUsernameError.Text = "Must be greater than 5";
+            }
+            else if (txtUsername.Text.Contains("<EOF>") || txtUsername.Text.Contains("|"))
+            {
+                lblDisplayUsernameError.Text = "Invalid Characters";
+            }
             else if (string.IsNullOrWhiteSpace(txtPassword.Text))
             {
                 lblDisplayPasswordError.Text = "Enter Password";
+            }
+            else if (txtPassword.Text.Length <= 5)
+            {
+                lblDisplayPasswordError.Text = "Must be greater than 5";
+            }
+            else if (txtPassword.Text.Contains("<EOF>") || txtPassword.Text.Contains("|"))
+            {
+                lblDisplayPasswordError.Text = "Invalid Characters";
             }
             else if (txtPassword.Text != txtPasswordConfirm.Text)
             {
@@ -164,42 +180,40 @@ namespace Chat1._0
             //Empty error message texts
             lblDisplayPasswordError.Text = String.Empty;
             lblDisplayUsernameError.Text = String.Empty;
-            // Username and password validation
-            if (!string.IsNullOrWhiteSpace(txtUsername.Text))
-            {
-                if (!string.IsNullOrWhiteSpace(txtPassword.Text))
-                {
 
-                    if (sctctrl.SendUserLoginRequest(txtUsername.Text, txtPassword.Text))
-                    {
-                        //Login successful-Open Chat Manager
-                        this.FormClosing -= this.loginForm_FormClosing;
-                        this.Close();
-                    }
-                    else
-                    {
-                        MessageBox.Show("Login Unsuccessful.");
-
-                        //For now have login go to chat manager for testing purposes
-                        //TODO remove before usage or when testing signup/login
-                        this.FormClosing -= this.loginForm_FormClosing;
-                        this.Close();
-                    }
-                    //for testing-delete when need to test login
-                    // This removes the application.exit event handler from the formclosing event;
-                    this.FormClosing -= this.loginForm_FormClosing;
-                    this.Close();
-                }
-                else
-                {
-                    lblDisplayPasswordError.Text = "Enter Password";
-                }
-
-            }
-            else
+            //username and password validation
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
             {
                 lblDisplayUsernameError.Text = "Enter Username";
             }
+            else if (txtUsername.Text.Contains("<EOF>") || txtUsername.Text.Contains("|"))
+            {
+                lblDisplayUsernameError.Text = "Invalid Characters";
+            }
+            else if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                lblDisplayPasswordError.Text = "Enter Password";
+            }
+            else if (txtPassword.Text.Contains("<EOF>") || txtPassword.Text.Contains("|"))
+            {
+                lblDisplayPasswordError.Text = "Invalid Characters";
+            }
+            else if (sctctrl.SendUserLoginRequest(txtUsername.Text, txtPassword.Text))
+            {
+                //Login successful-Open Chat Manager
+                this.FormClosing -= this.loginForm_FormClosing;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Login Unsuccessful.");
+
+                //For now have login go to chat manager for testing purposes
+                //TODO remove before usage or when testing signup/login
+                this.FormClosing -= this.loginForm_FormClosing;
+                this.Close();
+            }
+            
         }
 
 
